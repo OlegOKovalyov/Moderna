9cfbbe379c3c27c301dd80961bceb5d62ac044b7
OlegOKovqlyov <koa2003@ukr.net>
2018-08-28 18:30:35 +0300

    Moderna project first commit!

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+
+	@include('layouts.styles')
+
+	<!-- =======================================================
+    Theme Name: Moderna
+    Theme URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+	======================================================= -->
---
+++ resources/views/layouts/styles.blade.php
@@ -0,0 +1,19 @@
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<!-- css -->
+	<!-- <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+	<link href="{{ asset('css/bootstrap.min.css') }} " rel="stylesheet" />
+	<!-- <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> -->
+	<link href="{{ asset('css/fancybox/jquery.fancybox.css') }} " rel="stylesheet" />
+	<!-- <link href="css/jcarousel.css" rel="stylesheet" /> -->
+	<link href="{{ asset('css/jcarousel.css') }} " rel="stylesheet" />
+	<!-- <link href="css/flexslider.css" rel="stylesheet" /> -->
+	<link href="{{ asset('css/flexslider.css') }} " rel="stylesheet" />
+	<!-- <link href="css/style.css" rel="stylesheet" /> -->
+	<link href="{{ asset('css/style.css') }} " rel="stylesheet" />
+
+	<!-- Theme skin -->
+	<!-- <link href="skins/default.css" rel="stylesheet" /> -->
+	<link href="{{ asset('skins/default.css') }} " rel="stylesheet" />
